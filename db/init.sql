8b2a6bef82c8f8b05f27ed4c355f13b1dc4a7abf
katroue <katherine.demers@umontreal.ca>
2025-05-20 12:58:43 -0400

    Db created and docker connected to backend

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+create table if not exists Users (
+    id INT auto_increment primary key,
+    username  varchar(50) not null,
+    email varchar(100) not null,
+    password_hash varchar(255) not null
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    type ENUM('income', 'expense') NOT NULL,
+    category VARCHAR(50),
+    amount DECIMAL(10, 2),
+    date DATE,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
